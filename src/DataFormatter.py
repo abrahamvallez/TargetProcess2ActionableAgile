8f05f77a6fdefa501a9a6aa47fe76520644f6d85
Abraham Vallez <avallez@voxelgroup.net>
2020-02-19 14:20:33 +0100

    avoid back and forth in workflow

--- src/DataFormatter.py
+++ src/DataFormatter.py
@@ -19,17 +19,52 @@ class DataFormatter:
     def format_to_TP(self, data_list: list) -> dict:
         formatted_dict: dict = {}
 
-        for row in data_list:
-            user_story_id = row["currentUserStory"]['id']
-            user_story_state = row["entityState"]["name"]
-            date = row["date"]
-
-            formatted_row: dict = formatted_dict.get(user_story_id)
-            if user_story_state in USER_STORIES_STATES:
-                if formatted_row:
-                    formatted_row.update({user_story_state: date})
-                else:
-                    formatted_row = {'ID': user_story_id, user_story_state: date}
-                    formatted_dict[user_story_id] = formatted_row
+        for row_from_data in data_list:
+            self.format_row(formatted_dict, row_from_data)
 
         return formatted_dict
+
+    def format_row(self, formatted_dict, row_from_data: dict):
+        user_story_state = row_from_data["entityState"]["name"]
+
+        if user_story_state in USER_STORIES_STATES:
+            formatted_row: dict = formatted_dict.get(row_from_data["currentUserStory"]['id'])
+            if formatted_row and not self.is_a_step_back(formatted_row, row_from_data):
+                self.update_user_story(formatted_row, row_from_data)
+            elif not formatted_row:
+                self.add_new_user_story(formatted_dict, row_from_data)
+
+    def add_new_user_story(self, formatted_dict, row_from_data):
+        new_user_story = self.format_new_user_story(row_from_data)
+        formatted_dict[new_user_story['ID']] = new_user_story
+
+    def update_user_story(self, formatted_row, row_from_data):
+        user_story_state = row_from_data["entityState"]["name"]
+        date = row_from_data["date"]
+        if not formatted_row.get(user_story_state) or formatted_row.get(user_story_state) > date:
+            formatted_row.update({user_story_state: date})
+
+    def format_new_user_story(self, row_from_data: dict) -> dict:
+        user_story_id = row_from_data["currentUserStory"]['id']
+        user_story_state = row_from_data["entityState"]["name"]
+        date = row_from_data["date"]
+        return {
+            'ID': user_story_id,
+            user_story_state: date
+        }
+
+    def is_a_step_back(self, formatted_row: dict, row_from_data: dict) -> bool:
+        state_id = USER_STORIES_STATES.index(row_from_data["entityState"]["name"])
+
+        states_before = USER_STORIES_STATES[0:state_id-1]
+        for state in USER_STORIES_STATES:
+            if (formatted_row.get(state)
+                    and formatted_row.get(state) < row_from_data["date"]
+                    and state not in states_before):
+                return True
+
+        return False
+
+
+
+
--- tests/DataFormatter_test.py
+++ tests/DataFormatter_test.py
@@ -4,9 +4,9 @@ from src.DataFormatter import DataFormatter
 def test_formatted_data_has_a_field_named_ID():
     data_formatter = DataFormatter()
     data_list = [
-        {"date": "Date1", "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": "Open"}},
-        {"date": "Date2", "currentUserStory": {"id": 2, "name": "name2"}, "entityState": {"name": "Open"}},
-        {"date": "Date2", "currentUserStory": {"id": 3, "name": "name2"}, "entityState": {"name": "Open"}}
+        {"date": "2020-02-16T10:11:50.79", "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": "Open"}},
+        {"date": "2020-02-17T10:11:50.79", "currentUserStory": {"id": 2, "name": "name2"}, "entityState": {"name": "In Progress"}},
+        {"date": "2020-02-18T10:11:50.79", "currentUserStory": {"id": 3, "name": "name2"}, "entityState": {"name": "Done"}}
     ]
 
     formatted_data_dict = data_formatter.format_to_TP(data_list)
@@ -17,12 +17,13 @@ def test_formatted_data_has_same_ids_from_data_list():
     ids = [1, 2, 3]
     data_formatter = DataFormatter()
     data_list = [
-        {"date": "Date1", "currentUserStory": {"id": ids[0], "name": "name1"}, "entityState": {"name": "Open"}},
-        {"date": "Date2", "currentUserStory": {"id": ids[1], "name": "name2"}, "entityState": {"name": "Open"}},
-        {"date": "Date2", "currentUserStory": {"id": ids[2], "name": "name2"}, "entityState": {"name": "Open"}}
+        {"date": "2020-02-16T10:11:50.79", "currentUserStory": {"id": ids[0], "name": "name1"}, "entityState": {"name": "Open"}},
+        {"date": "2020-02-17T10:11:50.79", "currentUserStory": {"id": ids[1], "name": "name2"}, "entityState": {"name": "Open"}},
+        {"date": "2020-02-18T10:11:50.79", "currentUserStory": {"id": ids[2], "name": "name2"}, "entityState": {"name": "Open"}}
     ]
 
     formatted_data_dict = data_formatter.format_to_TP(data_list)
+
     assert formatted_data_dict.get(1).get('ID') == ids[0], "First element Id should be 1"
     assert formatted_data_dict.get(2).get('ID') == ids[1], "Second element Id should be 1"
     assert formatted_data_dict.get(3).get('ID') == ids[2], "Third element Id should be 1"
@@ -30,13 +31,18 @@ def test_formatted_data_has_same_ids_from_data_list():
 
 def test_dates_are_grouped_same_row_by_id():
     data_formatter = DataFormatter()
-    dates: list = ["Date1", "Date2", "Date3"]
+    dates: list = [
+        "2020-02-16T10:11:50.79",
+        "2020-02-17T10:11:50.79",
+        "2020-02-18T10:11:50.79"]
     data_list = [
         {"date": dates[0], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": "Open"}},
         {"date": dates[1], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": "In Progress"}},
         {"date": dates[2], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": "Done"}}
     ]
+
     formatted_data_dict = data_formatter.format_to_TP(data_list)
+
     values_in_formatted_data: list = formatted_data_dict.get(1).values()
     assert dates[0] in values_in_formatted_data, "Date1 should be in same row"
     assert dates[1] in values_in_formatted_data, "Date2 should be in same row"
@@ -45,13 +51,18 @@ def test_dates_are_grouped_same_row_by_id():
 
 def test_create_a_new_row_if_id_is_new():
     data_formatter = DataFormatter()
-    dates: list = ["Date1", "Date2", "Date3"]
+    dates: list = [
+        "2020-02-16T10:11:50.79",
+        "2020-02-17T10:11:50.79",
+        "2020-02-18T10:11:50.79"]
     data_list = [
         {"date": dates[0], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": "Open"}},
         {"date": dates[1], "currentUserStory": {"id": 2, "name": "name1"}, "entityState": {"name": "In Progress"}},
         {"date": dates[2], "currentUserStory": {"id": 2, "name": "name1"}, "entityState": {"name": "Done"}}
     ]
+
     formatted_data_dict = data_formatter.format_to_TP(data_list)
+
     values_in_formatted_data: list = formatted_data_dict.get(2).values()
     assert dates[1] in values_in_formatted_data, "Date2 should be in same row"
     assert dates[2] in values_in_formatted_data, "Date3 should be in same row"
@@ -59,7 +70,10 @@ def test_create_a_new_row_if_id_is_new():
 
 def test_dates_are_classified_by_state():
     data_formatter = DataFormatter()
-    dates: list = ["Date1", "Date2", "Date3"]
+    dates: list = [
+        "2020-02-16T10:11:50.79",
+        "2020-02-17T10:11:50.79",
+        "2020-02-18T10:11:50.79"]
     states: list = ["Open", "In Progress", "Done"]
     data_list = [
         {"date": dates[0], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[0]}},
@@ -68,8 +82,92 @@ def test_dates_are_classified_by_state():
     ]
 
     formatted_data_dict = data_formatter.format_to_TP(data_list)
-    assert formatted_data_dict.get(1).get(states[0]) == dates[0], "Open date should be Date1"
-    assert formatted_data_dict.get(1).get(states[1]) == dates[1], "Open date should be Date2"
-    assert formatted_data_dict.get(1).get(states[2]) == dates[2], "Open date should be Date3"
 
+    user_story_1_row: dict = formatted_data_dict.get(1)
+    assert user_story_1_row.get(states[0]) == dates[0], "Open date should be Date1"
+    assert user_story_1_row.get(states[1]) == dates[1], "Open date should be Date2"
+    assert user_story_1_row.get(states[2]) == dates[2], "Open date should be Date3"
+
+
+def test_keep_only_one_state_when_back_and_forth():
+    data_formatter = DataFormatter()
+    dates: list = [
+        "2020-02-16T10:11:50.79",
+        "2020-02-17T10:17:11.977",
+        "2020-02-18T10:13:35.86"]
+    states: list = ["Open", "In Progress"]
+    data_list = [
+        {"date": dates[0], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[0]}},
+        {"date": dates[1], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[1]}},
+        {"date": dates[2], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[0]}}
+    ]
+
+    formatted_data_dict = data_formatter.format_to_TP(data_list)
+    user_story_1_row: dict = formatted_data_dict.get(1)
+
+    assert list(user_story_1_row.keys()) == ["ID", "Open", "In Progress"], "Should have only two states"
+
+
+def test_keep_earlier_date_for_state_in_workflow_when_back_and_forth():
+    data_formatter = DataFormatter()
+    dates: list = [
+        "2020-02-16T10:11:50.79",
+        "2020-02-17T10:17:11.977",
+        "2020-02-18T10:13:35.86"]
+    states: list = ["Open", "In Progress"]
+    data_list = [
+        {"date": dates[0], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[0]}},
+        {"date": dates[1], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[1]}},
+        {"date": dates[2], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[0]}}
+    ]
+
+    formatted_data_dict = data_formatter.format_to_TP(data_list)
+    user_story_1_row: dict = formatted_data_dict.get(1)
+
+    assert user_story_1_row.get(states[0]) == '2020-02-16T10:11:50.79'
+    assert user_story_1_row.get(states[1]) == '2020-02-17T10:17:11.977'
+
+
+def test_keep_last_state_in_workflow_when_back_and_forth():
+    data_formatter = DataFormatter()
+    dates: list = ["2020-02-16T10:11:50.79",
+                   "2020-02-17T10:17:11.977",
+                   "2020-02-18T10:13:35.86",
+                   "2020-02-19T10:17:11.977"]
+    states: list = ["Open", "In Progress"]
+    data_list = [
+        {"date": dates[0], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[0]}},
+        {"date": dates[1], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[1]}},
+        {"date": dates[2], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[0]}},
+        {"date": dates[3], "currentUserStory": {"id": 1, "name": "name1"}, "entityState": {"name": states[1]}}
+    ]
+
+    formatted_data_dict = data_formatter.format_to_TP(data_list)
+    user_story_1_row: dict = formatted_data_dict.get(1)
+
+    assert user_story_1_row.get(states[0]) == '2020-02-16T10:11:50.79'
+    assert user_story_1_row.get(states[1]) == '2020-02-17T10:17:11.977'
+
+
+def test_keep_last_state_in_workflow_when_back_and_forth():
+    data_formatter = DataFormatter()
+    dates: list = ["2020-02-16T10:11:50.79",
+                   "2020-02-17T10:17:11.977",
+                   "2020-02-18T10:13:35.86",
+                   "2020-02-19T10:13:35.86",
+                   "2020-02-20T10:17:11.977"]
+    states: list = ["Open", "In Progress", "Pending DoD", "Done"]
+    data_list = [
+        {"date": dates[0], "currentUserStory": {"id": 10, "name": "name1"}, "entityState": {"name": states[0]}},
+        {"date": dates[1], "currentUserStory": {"id": 10, "name": "name1"}, "entityState": {"name": states[1]}},
+        {"date": dates[2], "currentUserStory": {"id": 10, "name": "name1"}, "entityState": {"name": states[3]}},
+        {"date": dates[3], "currentUserStory": {"id": 10, "name": "name1"}, "entityState": {"name": states[2]}},
+        {"date": dates[3], "currentUserStory": {"id": 10, "name": "name1"}, "entityState": {"name": states[3]}}
+    ]
+
+    formatted_data_dict = data_formatter.format_to_TP(data_list)
+    user_story_1_row: dict = formatted_data_dict.get(10)
+
+    assert user_story_1_row[states[3]] == '2020-02-18T10:13:35.86'
+    assert states[2] not in list(user_story_1_row.keys())
 
